cad55490499b7d8ecb6bf51c821fb4203f7f66a7
Haertel <otto.haertel@siemens.com>
2025-12-18 11:32:30 +0100

    feat: solving Day2

--- AdventOfCode2025/Day2/AoC2/IDChecker.cs
+++ AdventOfCode2025/Day2/AoC2/IDChecker.cs
@@ -2,5 +2,30 @@
 
 public class IDChecker
 {
-
+    public int CheckIDs(string input)
+    {
+        // Implementation goes here
+        int result =0;
+        List<string> idRanges = input.Split(',').ToList();
+        foreach(string id in idRanges)
+        {
+            int lowerBound = int.Parse(id.Split('-')[0]);
+            int upperBound = int.Parse(id.Split('-')[1]);
+            for(int i=lowerBound; i<=upperBound; i++)
+            {
+                string idStr = i.ToString();
+                if (idStr.Count() % 2 == 0)
+                {
+                    int middle = idStr.Length / 2;
+                    string firstHalf = idStr.AsSpan(0, middle).ToString();
+                    string secondHalf = idStr.AsSpan(middle).ToString();
+                    if(firstHalf.Equals(secondHalf))
+                    {
+                        result += i;
+                    }
+                }
+            }
+        }
+        return result;
+    }
 }
--- AdventOfCode2025/Day2/AoC2Test/IDCheckerTest.cs
+++ AdventOfCode2025/Day2/AoC2Test/IDCheckerTest.cs
@@ -1,4 +1,6 @@
-﻿namespace AoC2Test;
+﻿using AoC2;
+
+namespace AoC2Test;
 
 public class IDCheckerTest
 {
@@ -8,8 +10,18 @@ public class IDCheckerTest
     }
 
     [Test]
-    public void Test1()
+    [TestCaseSource(nameof(IDCheckerTestCases))]
+    public void Test1(string input, int additionOfAllInvalidIDs)
     {
-        Assert.Pass();
+        IDChecker idChecker = new IDChecker();
+        Assert.That(idChecker.CheckIDs(input).Equals(additionOfAllInvalidIDs));
+    }
+
+    private static IEnumerable<TestCaseData> IDCheckerTestCases()
+    {
+        yield return new TestCaseData(
+            "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124",
+            1227775554
+        ).SetName("Test with example input");
     }
 }
