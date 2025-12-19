caf4f20775bca553f0d072641707e9f33265fa9b
Haertel <otto.haertel@siemens.com>
2025-12-19 09:55:55 +0100

    feat: Day4

---
+++ AdventOfCode2025/Day4/AoC4/Forklift.cs
@@ -0,0 +1,64 @@
+﻿namespace AoC4;
+
+public class Forklift
+{
+    public int CountAccessibleRolls(string[] input)
+    {
+        int accessibleRolls = 0;
+        int localRools = 0;
+        List<List<string>> grid = new List<List<string>>();
+        foreach (string line in input)
+        {
+            grid.Add(line.Select(c => c.ToString()).ToList());
+        }
+        for (int i = 0; i < grid.Count; i++)
+        {
+            for (int j = 0; j < grid[i].Count; j++)
+            {
+
+                if (grid[i][j] == "@")
+                {
+                    localRools=0;
+                    if (i - 1 >= 0 && j - 1 >= 0 && grid[i - 1][j - 1] == "@")
+                    {
+                        localRools++;
+                    }
+                    if (i - 1 >= 0 && grid[i - 1][j] == "@")
+                    {
+                        localRools++;
+                    }
+                    if (i - 1 >= 0 && j + 1 < grid[i].Count && grid[i - 1][j + 1] == "@")
+                    {
+                        localRools++;
+                    }
+                    if (j - 1 >= 0 && grid[i][j - 1] == "@")
+                    {
+                        localRools++;
+                    }
+                    if (j + 1 < grid[i].Count && grid[i][j + 1] == "@")
+                    {
+                        localRools++;
+                    }
+                    if (i + 1 < grid.Count && j - 1 >= 0 && grid[i + 1][j - 1] == "@")
+                    {
+                        localRools++;
+                    }
+                    if (i + 1 < grid.Count && grid[i + 1][j] == "@")
+                    {
+                        localRools++;
+                    }
+                    if (i + 1 < grid.Count && j + 1 < grid[i].Count && grid[i + 1][j + 1] == "@")
+                    {
+                        localRools++;
+                    }
+                    if(localRools<4)
+                    {
+                        localRools=0;
+                        accessibleRolls++;
+                    }
+                }
+            }
+        }
+        return accessibleRolls;
+    }
+}
---
+++ AdventOfCode2025/Day4/AoC4Test/ForkliftTests.cs
@@ -0,0 +1,33 @@
+﻿using AoC4;
+
+namespace AoC4Test;
+
+public class ForkliftTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void CountAccisbleRoolsTest()
+    {
+        // Arrange
+        string[] input = new[]
+        {
+            "..@@.@@@@.",
+            "@@@.@.@.@@",
+            "@@@@@.@.@@",
+            "@.@@@@..@.",
+            "@@.@@@@.@@",
+            ".@@@@@@@.@",
+            ".@.@.@.@@@",
+            "@.@@@.@@@@",
+            ".@@@@@@@@.",
+            "@.@.@@@.@."
+        };
+        int expected = 13;
+        Forklift forklift = new Forklift();
+        Assert.That(forklift.CountAccessibleRolls(input), Is.EqualTo(expected));
+    }
+}
