a05194cde33fe21a17f5a7a2edd47f9e1a83b56d
Haertel <otto.haertel@siemens.com>
2026-01-12 15:16:29 +0100

    initial solution for day 7

--- AdventOfCode2025/Day7/AoC7/CalculatorSplitter.cs
+++ AdventOfCode2025/Day7/AoC7/CalculatorSplitter.cs
@@ -1,8 +1,8 @@
 namespace AoC7;
 public class CalculatorSplitter
 {
-    private char[,]input;
-    public CalculatorSplitter(char[,]Input)
+    private char[][]input;
+    public CalculatorSplitter(char[][]Input)
     {
         input = Input;
     }
@@ -13,20 +13,26 @@ public class CalculatorSplitter
         int splitCount = 0;
         for(int i = 1; i < input.GetLength(0); i++)
         {
-            for(int j = 0; j < input.GetLength(1); j++)
+            for(int j = 0; j < input[j].Count(); j++)
             {
-                if(input[i-1,j] == 'S' || input[i-1,j] == '|')
+                if(!input[i][j].Equals('^') && (input[i-1][j].Equals('S') || input[i - 1][j].Equals('|')))
                 {
-                    input[i,j]='|';
-                    splitCount++;
+                    input[i][j]='|';
                 }
-                if(i>0 && i<=input.GetLength(0) && j>0 && j<input.GetLength(1)-1)
+                if(i>0 && i<=input.GetLength(0) && j>0 && j<input[i].Count()-1)
                 {
-                    if(input[i,j]=='^')
+                    if(input[i][j].Equals('^'))
                     {
-                        input[i-1,j]='|';
-                        input[i+1,j] = '|';
-                        splitCount+=2;
+                        splitCount++;
+                        if (!input[i][j - 1].Equals('|'))
+                        {
+                            input[i][j - 1] = '|';
+                        }
+                        if(!input[i][j + 1].Equals('|'))
+                        {
+                            input[i][j + 1] = '|';
+                        }
+                        
                     }
                 }
             }
@@ -39,9 +45,9 @@ public class CalculatorSplitter
     {
         for(int i = 0; i < input.GetLength(0); i++)
         {
-            for(int j = 0; j < input.GetLength(1); j++)
+            for(int j = 0; j <  input[j].Count(); j++)
             {
-                Console.Write(input[i,j]);
+                Console.Write(input[i][j]);
             }
             Console.WriteLine();
         }
