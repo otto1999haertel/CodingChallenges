23c5940cdc38de45730eb336134bb1de43172ad2
Haertel <otto.haertel@siemens.com>
2026-01-13 14:52:54 +0100

    feat: Parser Day8

--- AdventOfCode2025/Day8/AoC8/DistanceCalculator.cs
+++ AdventOfCode2025/Day8/AoC8/DistanceCalculator.cs
@@ -2,5 +2,13 @@
 
 public class DistanceCalculator
 {
-
+    private string filePath;
+    public DistanceCalculator(string FilePath)
+    {
+        if (!File.Exists(FilePath))
+        {
+            throw new FileNotFoundException("The specified file was not found.", FilePath);
+        }
+        filePath = FilePath;
+    } 
 }
--- AdventOfCode2025/Day8/AoC8/Parser.cs
+++ AdventOfCode2025/Day8/AoC8/Parser.cs
@@ -1,6 +1,45 @@
+using System.Text;
+
 internal class Parser
 {
-    public void ParseInput(string filePathe)
+    private string filePath;
+
+    public Parser(string FilePath)
     {
+        if (!File.Exists(FilePath))
+        {
+            throw new FileNotFoundException("The specified file was not found.", FilePath);
+        }
+        filePath = FilePath;
+    }
+    public List<Point> ParseInput()
+    {
+        List<Point> points = new List<Point>();
+        using (var fileStream = File.OpenRead(filePath))
+        using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, 128))
+        {
+            string line;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                string[] strings = line.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                if (strings.Length != 3)
+                {
+                    throw new FormatException("Each line must contain exactly three comma-separated values.");
+                }
+
+                if (int.TryParse(strings[0], out int x) &&
+                    int.TryParse(strings[1], out int y) &&
+                    int.TryParse(strings[2], out int z))
+                {
+                    points.Add(new Point(x, y, z));
+                }
+                else
+                {
+                    throw new FormatException("All values must be valid integers.");
+                }
+            }
+        }
+
+        return points;
     }
 }
