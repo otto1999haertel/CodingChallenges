f6380fa6e7358a3b88cae7c1f92e99ef88beced6
ottohartel <otto.haertel@gmail.com>
2026-02-17 20:04:01 +0100

    chore: adapting naming

--- Python/CircleSquare.py
+++ Python/CircleSquare.py
@@ -1,5 +1,5 @@
 #https://edabit.com/challenge/NNhkGocuPMcryW7GP
-def square_areas_difference(radius):
+def _square_areas_difference(radius):
     a_bigger_square = (2*radius)**2
     a_smaller_suqare = ((2*radius)**2)/2
     return a_bigger_square-a_smaller_suqare
@@ -7,7 +7,7 @@ def square_areas_difference(radius):
 
 if __name__=="__main__":
     print("Circly Square Challenge")
-    assert(square_areas_difference(5)== 50)
-    assert(square_areas_difference(6)== 72)
-    assert(square_areas_difference(7)== 98)
-    assert(square_areas_difference(17)== 578)
+    assert(_square_areas_difference(5)== 50)
+    assert(_square_areas_difference(6)== 72)
+    assert(_square_areas_difference(7)== 98)
+    assert(_square_areas_difference(17)== 578)
